0ef11049c11af30d3ed000ff2b59b03bf54c6220
Kaaveh <kaaveh@pm.me>
2020-10-14 12:09:42 +0330

    Added User,Transaction and Address methods

--- tayeh-client/src/main/java/ir/pchas/tayehclient/network/MyAPI.kt
+++ tayeh-client/src/main/java/ir/pchas/tayehclient/network/MyAPI.kt
@@ -1,11 +1,24 @@
 package ir.pchas.tayehclient.network
 
 import ir.pchas.tayehclient.model.shop.Instance
+import ir.pchas.tayehclient.model.shop.Invoice
 import ir.pchas.tayehclient.model.shop.Product
+import ir.pchas.tayehclient.model.shop.Transaction
+import ir.pchas.tayehclient.model.user.Address
+import ir.pchas.tayehclient.model.user.Customer
+import ir.pchas.tayehclient.model.user.SubUser
 import ir.pchas.tayehclient.model.user.User
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.*
+
+private const val INSTANCE = "instance"
+private const val TRANSACTIONS = "transactions"
+private const val PRODUCT = "product"
+private const val INVOICES = "invoices"
+private const val CUSTOMER = "customer"
+private const val USER = "user"
+private const val ADDRESS = "addresses"
 
 internal interface MyAPI {
     companion object {
@@ -18,20 +31,15 @@ internal interface MyAPI {
         }
     }
 
-//    @GET("/user/me")
-//    suspend fun getUserMe(
-//        @Query("s") s: String
-//    ): User
-
     @GET("user/me")
     suspend fun getUserMe(@Header("Authorization") tokenString: String): User
 
-    @GET("user/instance/primary")
+    @GET("user/$INSTANCE/primary")
     suspend fun getInstancePrimary(@Header("Authorization") tokenString: String): Instance
 
     // Instance
 
-    @GET("instance/{id}")
+    @GET("$INSTANCE/{id}")
     suspend fun createInstancePrimary(
         @Path("id") instanceId: Int,
         @Header("Authorization") tokenString: String
@@ -43,13 +51,13 @@ internal interface MyAPI {
         @Header("Authorization") tokenString: String
     ): Instance
 
-    @POST("instance")
+    @POST(INSTANCE)
     fun createInstance(
         @Body instance: Instance,
         @Header("Authorization") tokenString: String
     )
 
-    @POST("instance/update")
+    @POST("$INSTANCE/update")
     fun updateInstance(
         @Body instance: Instance,
         @Header("Authorization") tokenString: String
@@ -57,30 +65,233 @@ internal interface MyAPI {
 
     // Product
 
-    @GET("instance/{id}/products")
+    @GET("$INSTANCE/{id}/products")
     fun getInstanceProducts(
         @Path("id") instanceId: Int,
         @Header("Authorization") tokenString: String
     ): List<Product>
 
-    @POST("instance/{id}/products")
+    @POST("$INSTANCE/{id}/products")
     fun createInstanceProduct(
         @Path("id") instanceId: Int,
         @Body product: Product,
         @Header("Authorization") tokenString: String
     )
 
-    @POST("instance/{id}/product/update")
+    @POST("$INSTANCE/{id}$PRODUCT/update")
     fun updateInstanceProduct(
         @Path("id") instanceId: Int,
         @Body product: Product,
         @Header("Authorization") tokenString: String
     )
 
-    @DELETE("instance/{id}/product/{product_id}}")
+    @DELETE("$INSTANCE/{id}$PRODUCT/{product_id}}")
     fun deleteInstanceProduct(
         @Path("id") instanceId: Int,
         @Path("product_id") productId: Int,
         @Header("Authorization") tokenString: String
     )
+
+    // Transaction
+
+    @GET("$INSTANCE/{id}$TRANSACTIONS")
+    fun getInstanceTransactions(
+        @Path("id") instanceId: Int,
+        @Header("Authorization") tokenString: String
+    ): List<Transaction>
+
+    @GET("$INSTANCE/{id}$TRANSACTIONS/open")
+    fun findInstanceTransactions(
+        @Path("id") instanceId: Int,
+        @Header("Authorization") tokenString: String
+    ): List<Transaction>
+
+    @GET("$INSTANCE/{id}$TRANSACTIONS$PRODUCT/{product_id}")
+    fun getProductTransactions(
+        @Path("id") instanceId: Int,
+        @Path("product_id") productId: Int,
+        @Header("Authorization") tokenString: String
+    ): List<Transaction>
+
+    @DELETE("$INSTANCE/{id}$TRANSACTIONS/{transaction_id}")
+    fun deleteInstanceTransaction(
+        @Path("id") instanceId: Int,
+        @Path("transaction_id") transactionId: Int,
+        @Header("Authorization") tokenString: String
+    )
+
+    @POST("$INSTANCE/{id}$TRANSACTIONS")
+    fun createInstanceTransaction(
+        @Path("id") instanceId: Int,
+        @Body transaction: Transaction,
+        @Header("Authorization") tokenString: String
+    )
+
+    @POST("$INSTANCE/{id}$TRANSACTIONS/update")
+    fun updateInstanceTransaction(
+        @Path("id") instanceId: Int,
+        @Body transaction: Transaction,
+        @Header("Authorization") tokenString: String
+    )
+
+    // Invoice
+
+    @GET("$INSTANCE/{id}/$INVOICES")
+    fun getInstanceInvoices(
+        @Path("id") instanceId: Int,
+        @Header("Authorization") tokenString: String
+    )
+
+    @GET("$INSTANCE/{id}/$INVOICES/{invoice_id")
+    fun getInstanceInvoice(
+        @Path("id") instanceId: Int,
+        @Path("invoice_id") invoiceId: Int,
+        @Header("Authorization") tokenString: String
+    )
+
+    @DELETE("$INSTANCE/{id}/$INVOICES/{invoice_id")
+    fun deleteInstanceInvoice(
+        @Path("id") instanceId: Int,
+        @Path("invoice_id") invoiceId: Int,
+        @Header("Authorization") tokenString: String
+    )
+
+    @POST("$INSTANCE/{id}/$INVOICES")
+    fun createInstanceInvoice(
+        @Path("id") instanceId: Int,
+        @Body invoice: Invoice,
+        @Header("Authorization") tokenString: String
+    )
+
+    @POST("$INSTANCE/{id}/$INVOICES/update")
+    fun updateInstanceInvoice(
+        @Path("id") instanceId: Int,
+        @Body invoice: Invoice,
+        @Header("Authorization") tokenString: String
+    )
+
+    @GET("$INSTANCE/{id}/series")
+    fun getInstanceSeries(
+        @Path("id") instanceId: Int,
+        @Header("Authorization") tokenString: String
+    ): List<Instance>
+
+    @GET("$INSTANCE/{id}/total/monthly")
+    fun getInstanceTotal(
+        @Path("id") instanceId: Int,
+        @Header("Authorization") tokenString: String
+    ): List<Instance>
+
+    @GET("$INSTANCE/{id}/revenue")
+    fun getInstanceRevenue(
+        @Path("id") instanceId: Int,
+        @Header("Authorization") tokenString: String
+    ): List<Instance>
+
+    // Customer
+
+    @GET("$INSTANCE/{id}/customers")
+    fun getInstanceCustomers(
+        @Path("id") instanceId: Int,
+        @Header("Authorization") tokenString: String
+    ): List<Customer>
+
+    @GET("$INSTANCE/{id}/$CUSTOMER/{customer_id}")
+    fun getInstanceCustomer(
+        @Path("id") instanceId: Int,
+        @Path("customer_id") customerId: Int,
+        @Header("Authorization") tokenString: String
+    ): Customer
+
+    @DELETE("$INSTANCE/{id}/$CUSTOMER/{customer_id}")
+    fun deleteInstanceCustomer(
+        @Path("id") instanceId: Int,
+        @Path("customer_id") customerId: Int,
+        @Header("Authorization") tokenString: String
+    )
+
+    @POST("$INSTANCE/{id}/$CUSTOMER")
+    fun createInstanceCustomer(
+        @Path("id") instanceId: Int,
+        @Body customer: Customer,
+        @Header("Authorization") tokenString: String
+    )
+
+    @POST("$INSTANCE/{id}/$CUSTOMER/update")
+    fun updateInstanceCustomer(
+        @Path("id") instanceId: Int,
+        @Body customer: Customer,
+        @Header("Authorization") tokenString: String
+    )
+
+    // User
+
+    @GET("$INSTANCE/{id}/users")
+    fun getInstanceUsers(
+        @Path("id") instanceId: Int,
+        @Header("Authorization") tokenString: String
+    ): List<SubUser>
+
+    @GET("$INSTANCE/{id}/$USER/{}user_id")
+    fun getInstanceUser(
+        @Path("id") instanceId: Int,
+        @Path("user_id") userId: Int,
+        @Header("Authorization") tokenString: String
+    ): SubUser
+
+    @DELETE("$INSTANCE/{id}/$USER/{user_id}")
+    fun deleteInstanceUser(
+        @Path("id") instanceId: Int,
+        @Path("user_id") userId: Int,
+        @Header("Authorization") tokenString: String
+    )
+
+    @POST("$INSTANCE/{id}/$USER")
+    fun createInstanceUser(
+        @Path("id") instanceId: Int,
+        @Body user: User,
+        @Header("Authorization") tokenString: String
+    )
+
+    @POST("$INSTANCE/{id}/$USER/update")
+    fun updateInstanceUser(
+        @Path("id") instanceId: Int,
+        @Body user: User,
+        @Header("Authorization") tokenString: String
+    )
+
+    // Addresses
+
+    @GET("$INSTANCE/{id}/addresses")
+    fun getInstanceAddresses(
+        @Path("id") instanceId: Int,
+        @Header("Authorization") tokenString: String
+    ): List<Address>
+
+    @DELETE("$INSTANCE/{id}/$ADDRESS/{address_id}")
+    fun deleteInstanceAddress(
+        @Path("id") instanceId: Int,
+        @Path("address_id") addressId: Int,
+        @Header("Authorization") tokenString: String
+    )
+
+    @POST("$INSTANCE/{id}/$ADDRESS")
+    fun createInstanceAddress(
+        @Path("id") instanceId: Int,
+        @Body address: Address,
+        @Header("Authorization") tokenString: String
+    )
+
+    @POST("$INSTANCE/{id}/$ADDRESS/update")
+    fun updateInstanceAddress(
+        @Path("id") instanceId: Int,
+        @Body address: Address,
+        @Header("Authorization") tokenString: String
+    )
+
+    @GET("$INSTANCE/page/{page_number}")
+    fun getInstancePage(
+        @Path("page_number") pageNumber: Int,
+        @Header("Authorization") tokenString: String
+    ): List<Instance>
 }
--- tayeh-client/src/main/java/ir/pchas/tayehclient/network/TayehClient.kt
+++ tayeh-client/src/main/java/ir/pchas/tayehclient/network/TayehClient.kt
@@ -1,7 +1,12 @@
 package ir.pchas.tayehclient.network
 
 import ir.pchas.tayehclient.model.shop.Instance
+import ir.pchas.tayehclient.model.shop.Invoice
 import ir.pchas.tayehclient.model.shop.Product
+import ir.pchas.tayehclient.model.shop.Transaction
+import ir.pchas.tayehclient.model.user.Address
+import ir.pchas.tayehclient.model.user.Customer
+import ir.pchas.tayehclient.model.user.SubUser
 import ir.pchas.tayehclient.model.user.User
 import java.io.IOException
 import kotlin.jvm.Throws
@@ -35,39 +40,187 @@ public class TayehClient(
 
     // Instance
     @Throws(IOException::class)
-    public suspend fun getInstance(instanceId: Int): Instance{
+    public suspend fun getInstance(instanceId: Int): Instance {
         return MyAPI().getInstance(instanceId, tokenString)
     }
 
     @Throws(IOException::class)
-    public fun createInstance(instance: Instance){
+    public fun createInstance(instance: Instance) {
         MyAPI().createInstance(instance, tokenString)
     }
 
     @Throws(IOException::class)
-    public fun updateInstance(instance: Instance){
+    public fun updateInstance(instance: Instance) {
         MyAPI().updateInstance(instance, tokenString)
     }
 
     // Product
     @Throws(IOException::class)
-    public fun getInstanceProducts(instanceId: Int): List<Product>{
+    public fun getInstanceProducts(instanceId: Int): List<Product> {
         return MyAPI().getInstanceProducts(instanceId, tokenString)
     }
 
     @Throws(IOException::class)
-    public fun createInstanceProduct(instanceId: Int, product: Product){
+    public fun createInstanceProduct(instanceId: Int, product: Product) {
         MyAPI().createInstanceProduct(instanceId, product, tokenString)
     }
 
     @Throws(IOException::class)
-    public fun updateInstanceProduct(instanceId: Int, product: Product){
+    public fun updateInstanceProduct(instanceId: Int, product: Product) {
         MyAPI().updateInstanceProduct(instanceId, product, tokenString)
     }
 
     @Throws(IOException::class)
-    public fun deleteInstanceProduct(instanceId: Int, productId: Int){
+    public fun deleteInstanceProduct(instanceId: Int, productId: Int) {
         MyAPI().deleteInstanceProduct(instanceId, productId, tokenString)
     }
 
+    // Transaction
+    @Throws(IOException::class)
+    public fun getInstanceTransactions(instanceId: Int): List<Transaction> {
+        return MyAPI().getInstanceTransactions(instanceId, tokenString)
+    }
+
+    @Throws(IOException::class)
+    public fun findInstanceTransactions(instanceId: Int): List<Transaction> {
+        return MyAPI().findInstanceTransactions(instanceId, tokenString)
+    }
+
+    @Throws(IOException::class)
+    public fun getProductTransactions(instanceId: Int, productId: Int): List<Transaction> {
+        return MyAPI().getProductTransactions(instanceId, productId, tokenString)
+    }
+
+    @Throws(IOException::class)
+    public fun deleteInstanceTransactions(instanceId: Int, transactionId: Int) {
+        MyAPI().deleteInstanceTransaction(instanceId, transactionId, tokenString)
+    }
+
+    @Throws(IOException::class)
+    public fun createInstanceTransaction(instanceId: Int, transaction: Transaction) {
+        MyAPI().createInstanceTransaction(instanceId, transaction, tokenString)
+    }
+
+    @Throws(IOException::class)
+    public fun updateInstanceTransaction(instanceId: Int, transaction: Transaction) {
+        MyAPI().updateInstanceTransaction(instanceId, transaction, tokenString)
+    }
+
+    @Throws(IOException::class)
+    public fun getInstanceInvoices(instanceId: Int) {
+        MyAPI().getInstanceInvoices(instanceId, tokenString)
+    }
+
+    @Throws(IOException::class)
+    public fun getInstanceInvoice(instanceId: Int, invoiceId: Int) {
+        MyAPI().getInstanceInvoice(instanceId, invoiceId, tokenString)
+    }
+
+    @Throws(IOException::class)
+    public fun deleteInstanceInvoice(instanceId: Int, invoiceId: Int) {
+        MyAPI().deleteInstanceInvoice(instanceId, invoiceId, tokenString)
+    }
+
+    @Throws(IOException::class)
+    public fun createInstanceInvoice(instanceId: Int, invoice: Invoice) {
+        MyAPI().createInstanceInvoice(instanceId, invoice, tokenString)
+    }
+
+    @Throws(IOException::class)
+    public fun updateInstanceInvoice(instanceId: Int, invoice: Invoice) {
+        MyAPI().updateInstanceInvoice(instanceId, invoice, tokenString)
+    }
+
+    @Throws(IOException::class)
+    public fun getInstanceSeries(instanceId: Int): List<Instance> {
+        return MyAPI().getInstanceSeries(instanceId, tokenString)
+    }
+
+    @Throws(IOException::class)
+    public fun getInstanceTotal(instanceId: Int): List<Instance> {
+        return MyAPI().getInstanceTotal(instanceId, tokenString)
+    }
+
+    @Throws(IOException::class)
+    public fun getInstanceRevenue(instanceId: Int): List<Instance> {
+        return MyAPI().getInstanceRevenue(instanceId, tokenString)
+    }
+
+    // Customer
+    @Throws(IOException::class)
+    public fun getInstanceCustomers(instanceId: Int): List<Customer> {
+        return MyAPI().getInstanceCustomers(instanceId, tokenString)
+    }
+
+    @Throws(IOException::class)
+    public fun getInstanceCustomer(instanceId: Int, customerId: Int): Customer {
+        return MyAPI().getInstanceCustomer(instanceId, customerId, tokenString)
+    }
+
+    @Throws(IOException::class)
+    public fun deleteInstanceCustomer(instanceId: Int, customerId: Int) {
+        MyAPI().deleteInstanceCustomer(instanceId, customerId, tokenString)
+    }
+
+    @Throws(IOException::class)
+    public fun createInstanceCustomer(instanceId: Int, customer: Customer) {
+        MyAPI().createInstanceCustomer(instanceId, customer, tokenString)
+    }
+
+    @Throws(IOException::class)
+    public fun updateInstanceCustomer(instanceId: Int, customer: Customer) {
+        MyAPI().updateInstanceCustomer(instanceId, customer, tokenString)
+    }
+
+    // User
+    @Throws(IOException::class)
+    public fun getInstanceUsers(instanceId: Int): List<SubUser> {
+        return MyAPI().getInstanceUsers(instanceId, tokenString)
+    }
+
+    @Throws(IOException::class)
+    public fun getInstanceUser(instanceId: Int, userId: Int): SubUser {
+        return MyAPI().getInstanceUser(instanceId, userId, tokenString)
+    }
+
+    @Throws(IOException::class)
+    public fun deleteInstanceUser(instanceId: Int, userId: Int) {
+        MyAPI().deleteInstanceUser(instanceId, userId, tokenString)
+    }
+
+    @Throws(IOException::class)
+    public fun createInstanceUser(instanceId: Int, user: User) {
+        MyAPI().createInstanceUser(instanceId, user, tokenString)
+    }
+
+    @Throws(IOException::class)
+    public fun updateInstanceUser(instanceId: Int, user: User) {
+        MyAPI().updateInstanceUser(instanceId, user, tokenString)
+    }
+
+    // Address
+    @Throws(IOException::class)
+    public fun getInstanceAddresses(instanceId: Int): List<Address> {
+        return MyAPI().getInstanceAddresses(instanceId, tokenString)
+    }
+
+    @Throws(IOException::class)
+    public fun deleteInstanceAddress(instanceId: Int, addressId: Int) {
+        MyAPI().deleteInstanceAddress(instanceId, addressId, tokenString)
+    }
+
+    @Throws(IOException::class)
+    public fun createInstanceAddress(instanceId: Int, address: Address) {
+        MyAPI().createInstanceAddress(instanceId, address, tokenString)
+    }
+
+    @Throws(IOException::class)
+    public fun updateInstanceAddress(instanceId: Int, address: Address) {
+        MyAPI().updateInstanceAddress(instanceId, address, tokenString)
+    }
+
+    @Throws(IOException::class)
+    public fun getInstancePage(pageNumber: Int): List<Instance> {
+        return MyAPI().getInstancePage(pageNumber, tokenString)
+    }
 }
